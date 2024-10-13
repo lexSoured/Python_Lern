9bf280cf223f6e62b27caa2a2714fba724958d0c
lexsoured <lexsoured@gmail.com>
2024-10-13 09:13:38 +0300

    Создана функция установки времени

--- project01.py
+++ project01.py
@@ -1,4 +1,22 @@
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
+import datetime 
+
+def set():
+    remind = simpledialog.askstring('Reminder time', 'Enter reminder time format HH:MM(#in 24 hour clock)')
+    if remind:
+        try:
+            hour = int(remind.split(':')[0])
+            minute = int(remind.split(':')[1])
+            now = datetime.datetime.now()
+            print(now)
+            time_rem = now.replace(hour=hour, minute=minute)
+            print(time_rem)
+            t_stamp = time_rem.timestamp()
+            print(time_rem, t_stamp)
+        except Exception as e:
+            messagebox.showerror('Error', {e})
 
 root = tk.Tk()
 root.title('Reminder')
